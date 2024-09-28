52740004f02e4a1d9ade657e19cf7a1060bdef24
HubiAtGit <hubertchristoph.baechli@uzh.ch>
2024-09-28 21:44:45 +0200

    hw2

--- hw2/ownVersion.R
+++ hw2/ownVersion.R
@@ -84,7 +84,6 @@ plot(df$ID,df$nE,main="Number of Changes",
      col=rgb(1,0,0,0.2))
 
 
-<<<<<<< HEAD
 plot(df$nE,df$Me,main="Correlation to the Number of Changes",
      xlab="Number of Changes",ylab="Money at the End",
      col=rgb(1,0,0,0.2))
@@ -129,7 +128,6 @@ legend('topright', c('Money befor', 'Money after'),
        fill=c(rgb(0,0,1,0.5), rgb(0,1,0,0.2)))
 
 
-=======
 plot(df$nE,df$Me,main="Correlation?",
      xlab="Number of Changes",ylab="Money at the End",
      col=rgb(1,0,0,0.2))
@@ -141,4 +139,40 @@ plot(df$Ms,df$Me,main="Correlation?",
 plot(df$ID,df$Me,main="Correlation?",
      xlab="ID",ylab="Money at the End",
      col=rgb(1,0,0,0.2))
->>>>>>> be86c75b75f21718c91c65aff3cf02d31c29d0a8
+
+SplitRandomR <- function(nAgent,maxExchange,Money_mean=1000,Money_sd=300) {
+  df <- data.frame(ID=seq(1,nAgent),
+                   nE=0,
+                   Ms=rnorm(nAgent, mean = Money_mean, sd = Money_sd),
+                   MT=0,Me=0)
+  df$Me <- df$Ms
+  iE <- 1
+  while (iE <= maxExchange) {
+    df$MT <- df$Me
+    rdf <- sample(df[df$nE < iE,"ID"], size=2)
+    rds <- sample(0:100, size=1)/100
+    df[(df$ID == rdf[1] | df$ID == rdf[2]),"nE"] <-
+      df[(df$ID == rdf[1] | df$ID == rdf[2]),"nE"]+1
+    df[(df$ID == rdf[1] | df$ID == rdf[2]),"Me"] <-
+      sum(df[(df$ID == rdf[1] | df$ID == rdf[2]),"MT"])*c(rds,1-rds)
+    if (NROW(df[df$nE < iE,"ID"]) < 2) {
+      iE <- iE+1
+    }
+  }
+  return(df)
+}
+
+df <- SplitRandomR(1000,30)
+
+summary(df)
+hist(df$Ms, xlab="Money",main="Histogram for split-random Restricted",
+     col=rgb(0,0,1,0.2),
+     xlim=c(0,max(c(df$Me,df$Ms)))*1.1,ylim=c(0,nrow(df)/2))
+hist(df$Me, col=rgb(0,1,0,0.2),
+     add=TRUE)
+legend('topright', c('Money befor', 'Money after'),
+       fill=c(rgb(0,0,1,0.5), rgb(0,1,0,0.2)))
+
+plot(df$ID,df$nE,main="Number of Changes",
+     xlab="ID",ylab="Number of Changes",
+     col=rgb(1,0,0,0.2))
